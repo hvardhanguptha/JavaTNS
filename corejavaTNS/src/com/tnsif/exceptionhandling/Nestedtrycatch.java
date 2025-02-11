b244b2110b14736f43a45c9c5c1776867e7c94f9
hvardhanguptha <harshavardhan.152159@gmail.com>
2025-02-11 10:05:16 +0530

    second commit

--- corejavaTNS/src/com/tnsif/exceptionhandling/Nestedtrycatch.java
+++ corejavaTNS/src/com/tnsif/exceptionhandling/Nestedtrycatch.java
@@ -1,12 +1,21 @@
 package com.tnsif.exceptionhandling;
 
+import java.io.IOException;
+
 public class Nestedtrycatch {
 	public static void check() {
 		try {
 			try {
-				
+				System.out.println("hello");
 			}
+		}
+		catch(ArithmeticException e) {
+				System.out.println("hi"+ e.getMessage());
+			}
+		catch(IOException a) {
+			System.out.println("hi");
+		}
+		
 		}
 	}
 
-}
